2e5b24e56cc4827472558a917f56c3f8c6654596
Kirill Strelkov <kirillstrelkov@users.noreply.github.com>
2016-03-25 20:14:31 +0100

    optizimed using PageSpeed Insights

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -12,3 +12,23 @@ function logCRP() {
 window.addEventListener("load", function(event) {
   logCRP();
 });
+
+var cb = function() {
+  var l = document.createElement('link'); l.rel = 'stylesheet';
+  l.href = 'css/style.css';
+  var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
+};
+var cb2 = function() {
+  var l = document.createElement('link'); l.rel = 'stylesheet';
+  l.href = '//fonts.googleapis.com/css?family=Open+Sans:400,700';
+  var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
+};
+
+var raf = requestAnimationFrame || mozRequestAnimationFrame ||
+    webkitRequestAnimationFrame || msRequestAnimationFrame;
+
+if (raf){
+  raf(cb);
+} else {
+  window.addEventListener('load', cb2);
+}
